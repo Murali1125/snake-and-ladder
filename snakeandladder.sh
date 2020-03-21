81083bba3e0266664a7a1092363e8abe95a624a0
murali1125 <murali1125s@gmail.com>
2020-03-21 22:39:13 +0530

    uc1

--- snakeandladder.sh
+++ snakeandladder.sh
@@ -1,1 +1,17 @@
 #! /bin/bash -x
+
+
+# Snake and ladder game played with single player at stsrt possion 0
+#-------------------------------------------------------------------------------------
+
+
+function snake_and_ladder() {
+	#count variable to count the dice rolls
+	count=0
+	#possition of the player
+	possition=0
+	player=player01
+	# declare players variables
+	player1_pos=0
+
+}
